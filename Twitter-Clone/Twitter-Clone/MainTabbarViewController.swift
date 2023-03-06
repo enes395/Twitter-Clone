4c4474f1667f4a668daffbfdea792d4be4e0b3e0
Muhammed Enes <kiliclimuhammedenes@gmail.com>
2023-03-06 13:58:54 +0300

    Remove Main Storyboard

---
+++ Twitter-Clone/Twitter-Clone/MainTabbarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter-Clone
+//
+//  Created by Muhammed Enes Kılıçlı on 6.03.2023.
+//
+
+import UIKit
+
+class MainTabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
